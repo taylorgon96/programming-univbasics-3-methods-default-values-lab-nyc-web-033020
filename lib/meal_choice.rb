a0e50abceb753111b8d30beb12050b7c3e945657
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-26 21:53:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(veg1, veg2, meat = "protein")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}." so that you can also see what you've ordered.
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
